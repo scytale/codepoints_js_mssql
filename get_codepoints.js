f0921503f332461cb1eb4c1042aab2ae24099f95
scytale <huari4@gmail.com>
2014-05-30 15:30:17 -0700

    Create get_codepoints.js
    hard coded num char reference values

---
+++ get_codepoints.js
@@ -0,0 +1,22 @@
+//get a range of num char refs http://www.w3.org/MarkUp/html3/latin1.html
+//edit values the the for loop condition
+//paste back to ms sql server 2008 r2
+
+
+var result="";
+
+function codePointValue(number){
+  return String.fromCodePoint(number);
+};
+for (var i=241;i<246;i++){
+  if (codePointValue(i) == ""){
+    console.log("no value");
+  } else {
+      console.log(i + " is " + codePointValue(i));
+      console.log("SELECT Id,TopicText,Description from Topic where TopicText like '%[" + codePointValue(i) + "]%' Collate SQL_Latin1_General_CP1_CS_AS" );
+      console.log("UPDATE Topic SET TopicText=REPLACE(TopicText,'"+ codePointValue(i) + "','&#" + i + ";') WHERE Id in (" + "InsertTopicIds)")
+      result = result + codePointValue(i);
+  }
+}
+
+console.log("SELECT Id,TopicText,Description from Topic where TopicText like '%[" + result + "]%' Collate SQL_Latin1_General_CP1_CS_AS" );
